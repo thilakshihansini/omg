c45f1eee15967a180459bb6c7da8aee70e604ba0
Thilakshi Hansini <thilakshihansini@Thilakshis-MacBook-Air.local>
2024-08-11 11:36:22 +0930

    omg

---
+++ src/main/java/com/mycompany/omg/Omg.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.omg;
+
+/**
+ *
+ * @author thilakshihansini
+ */
+public class Omg {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
